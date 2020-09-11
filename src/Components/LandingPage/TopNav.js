4dc8205896a4355ea23d3a25681ee2bca33636d7
sandip satyal <sandipsatyal25@gmail.com>
2020-09-11 19:06:33 +0545

    [add] topNav desktop view

--- src/Components/LandingPage/TopNav.js
+++ src/Components/LandingPage/TopNav.js
@@ -1,14 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  DownloadOutlined,
+  DesktopOutlined,
+  MonitorOutlined,
+  PhoneOutlined,
+  SolutionOutlined,
+} from "@ant-design/icons";
 
 const TopNav = () => {
   return (
-    <div className="d-flex justify-content-end">
-      <ul className="d-flex">
-        <li>Download</li>
-        <li>Software</li>
-        <li>PU Results</li>
-        <li>Carrer</li>
-        <li>Contact Us</li>
+    <div className="d-flex justify-content-end top-nav">
+      <ul className="d-flex top-nav__list">
+        <li className="top-nav__list--list-item">
+          <Link to="#">
+            <DownloadOutlined /> Downloads
+          </Link>
+        </li>
+        <li className="top-nav__list--list-item">
+          <Link to="#">
+            <DesktopOutlined /> Softwares
+          </Link>
+        </li>
+        <li className="top-nav__list--list-item">
+          <Link to="#">
+            <SolutionOutlined /> PU Results
+          </Link>
+        </li>
+        <li className="top-nav__list--list-item">
+          <Link to="#">
+            <MonitorOutlined /> Carrers
+          </Link>
+        </li>
+        <li className="top-nav__list--list-item">
+          <Link to="#">
+            <PhoneOutlined />
+            Contact Us
+          </Link>
+        </li>
       </ul>
     </div>
   );
