e7940a6aa815b63d046e649879896bf2325e7427
sandip satyal <sandipsatyal25@gmail.com>
2020-09-11 23:59:20 +0545

    [add] antd style.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap-css-only";
+import "../node_modules/antd/dist/antd.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
